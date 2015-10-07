fce54434bf6f25f625cb43447f484a5202a4b6ac
Jose Ruiz <jruizj6@gmail.com>
2015-10-06 19:24:23 -0600

    Cargar datos desde catálogo(por ahora solo en pais de crear evento)

---
+++ catalogo_pais.php
@@ -0,0 +1,13 @@
+<?php
+$conn = oci_connect('mmAdmin','mmAdmin', '//localhost/MATCHMEDB');
+if (!$conn) {
+	$error = "No se pudo conectar con la base de datos";
+} else {
+	$stid = oci_parse($conn,'select pais_id,pais_nombre from pais');
+    oci_execute($stid);
+    while (($row = oci_fetch_array($stid, OCI_ASSOC)) != false) {
+               echo "<option value=".$row['PAIS_ID'].">".$row['PAIS_NOMBRE']."</option>";
+            }
+	oci_close($conn);
+}
+?>
