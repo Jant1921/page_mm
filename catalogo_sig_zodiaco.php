78d953777339f7b39684654bfcede765f7cd0c3a
Daniel <>
2015-10-06 21:36:21 -0600

    creacion de catalogo_szodiaco para mostrar los signos zodiacales, y su implementacion en la pagregdatespecif, ademas de asignar valores para insersion a distintos droplist.

---
+++ catalogo_sig_zodiaco.php
@@ -0,0 +1,13 @@
+<?php
+$conn = oci_connect('mmAdmin','mmAdmin', '//localhost/MATCHMEDB');
+if (!$conn) {
+	$error = "No se pudo conectar con la base de datos";
+} else {
+	$stid = oci_parse($conn,'select SIGNOZODIACO_ID,SIGNOZODIACO_NOMBRE from SIGNOZODIACO');
+    oci_execute($stid);
+    while (($row = oci_fetch_array($stid, OCI_ASSOC)) != false) {
+               echo "<option value=".$row['SIGNOZODIACO_ID'].">".$row['SIGNOZODIACO_NOMBRE']."</option>";
+            }
+	oci_close($conn);
+}
+?>
