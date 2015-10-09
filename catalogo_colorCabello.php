b2bd63c6d319ee54de5489ecd4d478d8dfb13b7e
Jose Ruiz <jruizj6@gmail.com>
2015-10-09 09:10:32 -0600

    Corrección de errores en catalogos

--- catalogo_colorCabello.php
+++ catalogo_colorCabello.php
@@ -3,7 +3,7 @@ $conn = oci_connect('mmAdmin','mmAdmin', '//localhost/MATCHMEDB');
 if (!$conn) {
 	$error = "No se pudo conectar con la base de datos";
 } else {
-	$stid = oci_parse($conn,'colorpelo_id,colorpelo_nombre from colorpelo');
+	$stid = oci_parse($conn,'select colorpelo_id,colorpelo_nombre from colorpelo');
     oci_execute($stid);
     while (($row = oci_fetch_array($stid, OCI_ASSOC)) != false) {
                echo "<option value=".$row['COLORPELO_ID'].">".$row['COLORPELO_NOMBRE']."</option>";
